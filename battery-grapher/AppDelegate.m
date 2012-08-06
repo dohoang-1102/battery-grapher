7943c99eff0fdd105d8d67c97cd9c62ec7000529
Sean Kelley <seansfkelley@gmail.com>
2012-08-05 23:09:56 -0700

    Initial commit.

---
+++ battery-grapher/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  battery-grapher
+//
+//  Created by Sean Kelley on 8/5/12.
+//  Copyright (c) 2012 Sean Kelley. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ battery-grapher/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  battery-grapher
+//
+//  Created by Sean Kelley on 8/5/12.
+//  Copyright (c) 2012 Sean Kelley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
